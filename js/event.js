0c0a59044e70262228eba2c008883b5edab00b8f
Algo Ren <algorenstudio@gmail.com>
2020-12-11 20:31:25 -0500

    I've mostly been playing around with the layout of the website using CSS grid really getting indepth of the capabilities and how properties can be mixed and matched to get the result that I'm looking for. I've finally got one of the page sections, poolWork, to act and look how I want on ultra res/wide screens and on laptop or narrow window screens. I'll be implementing the same media queries on the poolDeck section as well. I've also added a gallery section in pageContent that has a unique image layout. I spent so much time tinkering with this and honestly the code probably needs to be cleaned up a bit, but I like the resulting product. Finally, I used event.js to do some dynamic page button handling where if the user is on the same page that the click to navigate to the page wont be refreshed. My thinking is to cut down on page request, hence potential data sending, and also when going to implement page transition animations not loading an animation for no reason. Right now this feature is a bit buggy but I'm getting a handle on it.

--- js/event.js
+++ js/event.js
@@ -1,6 +1,11 @@
 // Loading in elements
 branding = $("#branding");
-// Listening for button click
+poolSectionHeader = $("#poolContainer span:last-of-Type");
+deckSectionHeader = $("#decksContainer span:last-of-Type");
+poolSectionButtons = document.querySelectorAll(".nav-item[id^='pool'] ");
+console.log(poolSectionHeader);
+console.log(poolSectionButtons);
+// Listening for button click (with DOM)
 window.onclick = (e) => {
   e.preventDefault();
   // Full href of clicked on link
@@ -13,16 +18,53 @@ window.onclick = (e) => {
     comparePage(e.target.href);
   } else {
     // Run function to toggle between work and material section
-    console.log(e.target.parentNode.id, "Section toggle");
+    console.log(e.target.parentNode.id, " Section toggle");
+    console.log(
+      e.target.parentElement.nextSibling,
+      " Sibling of parent element"
+    );
+    console.log();
     goTo(e.target.parentNode.id, true);
   }
 };
+
+// Listening for button click (with jQuery)
+$(poolSectionButtons).click((e) => {
+  console.log("The Jquery way");
+  console.log(e.target);
+  console.log(e.target.parentElement.id);
+  poolSectionButtons.forEach((button) => {
+    // Does this button equal the clicked on li?
+    console.log(button, " in this function");
+    console.log(button.id, " the child element of button");
+    console.log(button.children[0], " the p tag");
+    if (button.id === e.target.parentElement.id) {
+      console.log(true);
+      $(button.children[0]).addClass("active");
+    } else {
+      console.log(false);
+      $(button.children[0]).removeClass("active");
+    }
+  });
+  // console.log(e.target.parentNode);
+  // if (e.target.classList.contains("active")) {
+  //   console.log("Is working");
+  //   console.log(e.target.classList);
+  //   console.log(e.target.parentNode.classList);
+  // } else {
+  //   console.log("Not working");
+  // }
+});
 
 // Get current page
 const comparePage = (thisPage) => {
   // Page the user is on.
   let page = window.location.href;
   console.log("Current page: ", page);
+  console.log(
+    "Page starts with: ",
+    window.location.href.substring(0, page.indexOf("#"))
+  );
 
   if (thisPage === page) {
     console.log("page not redirected");
@@ -51,6 +93,7 @@ const goTo = (page, section) => {
     // Determine what page section. Either pool or deck.
     // Add and remove active class to change button look and hide/unhide content.
     console.log("Will just load section");
+    changeHeader(page);
   }
 };
 
@@ -58,3 +101,25 @@ const goTo = (page, section) => {
 // Either by hidding and unhiding the html structure
 // or dynamically replacing the elements within the page...
 // This will be fun, and may be better for reduced initial load times?
+
+const changeHeader = (page) => {
+  console.log(page);
+  switch (page) {
+    case "poolMaterial":
+      poolSectionHeader[0].innerText = "Material";
+      break;
+    case "poolWork":
+      poolSectionHeader[0].innerText = "Work";
+      break;
+    case "deckMaterial":
+      deckSectionHeader[0].innerText = "Material";
+      break;
+    case "deckWork":
+      deckSectionHeader[0].innerText = "Work";
+      break;
+    default:
+      break;
+  }
+};
+
+const applyActiveToButton = () => {};
