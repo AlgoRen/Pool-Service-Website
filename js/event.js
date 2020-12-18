2bd7f5efd32e2cba1eb4e1a1f44177d19a3fcb9a
Algo Ren <algorenstudio@gmail.com>
2020-12-17 22:39:26 -0500

    Right now the site is close to completition, it has a working navbar for desktop and mobile, the responsive layout is all but polished, and colors and font styles are starting to be aesthetically tuned, as well as adding the words I find best suite the site as requested by my client.

--- js/event.js
+++ js/event.js
@@ -1,13 +1,14 @@
 // Loading in elements
 branding = $("#branding");
 poolSectionHeader = $("#poolContainer span:last-of-Type");
-deckSectionHeader = $("#decksContainer span:last-of-Type");
+deckSectionHeader = $("#deckContainer span:last-of-Type");
 poolSectionButtons = document.querySelectorAll(".nav-item[id^='pool'] ");
+deckSectionButtons = document.querySelectorAll(".nav-item[id^='deck'] ");
 console.log(poolSectionHeader);
 console.log(poolSectionButtons);
 // Listening for button click (with DOM)
 window.onclick = (e) => {
-  e.preventDefault();
+  // e.preventDefault();
   // Full href of clicked on link
   const isaNavItem = e.target.parentNode.classList.contains("nav-item");
   if (!isaNavItem) {
@@ -15,7 +16,7 @@ window.onclick = (e) => {
     return;
   } else if (e.target.href) {
     console.log("Target link: ", e.target.href);
-    comparePage(e.target.href);
+    // comparePage(e.target.href);
   } else {
     // Run function to toggle between work and material section
     console.log(e.target.parentNode.id, " Section toggle");
@@ -23,77 +24,49 @@ window.onclick = (e) => {
       e.target.parentElement.nextSibling,
       " Sibling of parent element"
     );
-    console.log();
-    goTo(e.target.parentNode.id, true);
+    goTo(null, e.target.parentNode.id, true);
   }
 };
 
 // Listening for button click (with jQuery)
 $(poolSectionButtons).click((e) => {
-  console.log("The Jquery way");
-  console.log(e.target);
-  console.log(e.target.parentElement.id);
-  poolSectionButtons.forEach((button) => {
-    // Does this button equal the clicked on li?
-    console.log(button, " in this function");
-    console.log(button.id, " the child element of button");
-    console.log(button.children[0], " the p tag");
-    if (button.id === e.target.parentElement.id) {
-      console.log(true);
-      $(button.children[0]).addClass("active");
-    } else {
-      console.log(false);
-      $(button.children[0]).removeClass("active");
-    }
-  });
-  // console.log(e.target.parentNode);
-  // if (e.target.classList.contains("active")) {
-  //   console.log("Is working");
-  //   console.log(e.target.classList);
-  //   console.log(e.target.parentNode.classList);
-  // } else {
-  //   console.log("Not working");
-  // }
+  e.preventDefault();
+  applyActiveToButton(e);
+});
+
+$(deckSectionButtons).click((e) => {
+  e.preventDefault();
+  applyActiveToButton(e);
 });
 
 // Get current page
-const comparePage = (thisPage) => {
+const comparePage = (targetPage) => {
   // Page the user is on.
-  let page = window.location.href;
-  console.log("Current page: ", page);
-  console.log(
-    "Page starts with: ",
-    window.location.href.substring(0, page.indexOf("#"))
-  );
+  let currentPage = window.location.href;
+  console.log("Current page: ", currentPage);
+  console.log("Target Page: ", targetPage);
 
-  if (thisPage === page) {
+  if (targetPage === currentPage) {
     console.log("page not redirected");
   } else {
-    let pageString = thisPage.substring(thisPage.lastIndexOf("/") + 1);
-    console.log(pageString);
-    goTo(pageString, false);
+    // See if the page starts with a # or contains .html in its href.
+    // If so do something for each of those cases.
+
+    goTo(currentPage, targetPage, false);
   }
-  //   console.log(page.substring(page.lastIndexOf("/") + 1));
-  // Full href user wants to go to.
-  //   console.log("Page user wants to go to: ", $(this).find("a")[0].href);
-  // goTo(page);
-  //   });
 };
 
-const goTo = (page, section) => {
+const goTo = (currentPage, targetPage, section) => {
   // Determine if page is going to a brand new page
   // or if page is loading a new section within a page.
   if (!section) {
-    console.log("Will just load new page....");
     // Send user to appropiate page
-    console.log(window.location.host + "/" + page, "getting better");
-    const newURL = "/" + page;
-    window.location.replace(newURL);
+    window.location.replace(currentPage, targetPage);
   } else {
     // Determine what page section. Either pool or deck.
     // Add and remove active class to change button look and hide/unhide content.
     console.log("Will just load section");
-    changeHeader(page);
+    changeHeader(targetPage);
   }
 };
 
@@ -122,4 +95,39 @@ const changeHeader = (page) => {
   }
 };
 
-const applyActiveToButton = () => {};
+const toggleSections = (show) => {
+  // if workContainer does not have hidden class then show()
+  // else add hidden class to materialContainer
+  // if workContainer does have hidden class then hide()
+  // and show materialContainer and remove hidden class
+};
+
+const applyActiveToButton = (e) => {
+  poolSectionButtons.forEach((button) => {
+    // Does this button equal the clicked on li?
+    console.log(button.id, " the child element of button");
+    console.log(button.children[0], " the p tag");
+    if (button.id === e.target.parentElement.id) {
+      console.log(true);
+      $(button.children[0]).addClass("active");
+    } else {
+      console.log(false);
+      $(button.children[0]).removeClass("active");
+    }
+    // Add hidden class from this elements parent container
+    // Remove class from the sibling parent container
+  });
+  deckSectionButtons.forEach((button) => {
+    // Does this button equal the clicked on li?
+    console.log(button, " in this function");
+    console.log(button.id, " the child element of button");
+    console.log(button.children[0], " the p tag");
+    if (button.id === e.target.parentElement.id) {
+      console.log(true);
+      $(button.children[0]).addClass("active");
+    } else {
+      console.log(false);
+      $(button.children[0]).removeClass("active");
+    }
+  });
+};
